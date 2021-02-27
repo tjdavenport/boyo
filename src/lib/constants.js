e9b5fb5afdcff34c76bbe6d5a14d9efca2dcf1aa
tjdavenport <tjdavenport2@gmail.com>
2021-02-27 16:21:21 -0500

    define auto factions constants

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,9 +1,40 @@
 const dPerms = {
+  addReactions: 0x00000040,
   readMessageHistory: 0x00010000,
   sendMessages: 0x00000800,
+
+  manageRoles: 0x10000000,
+
+  manageChannels: 0x00000010,
+  viewChannel: 0x00000400,
 };
 
 exports.categories = {
+  'auto-factions': {
+    name: 'Auto Factions',
+    botCommands: {
+      'create-faction': {
+        description: 'Create a faction role.',
+        discordPerms: [dPerms.readMessageHistory, dPerms.sendMessages, dPerms.addReactions, dPerms.manageRoles, dPerms.manageChannels, dPerms.viewChannel],
+        oAuth2Links: [],
+      },
+      'faction-invite': {
+        description: 'Invite someone to your faction.',
+        discordPerms: [dPerms.readMessageHistory, dPerms.sendMessages, dPerms.addReactions, dPerms.manageRoles, dPerms.manageChannels, dPerms.viewChannel],
+        oAuth2Links: [],
+      },
+      'faction-kick': {
+        description: 'Kick someone from your faction.',
+        discordPerms: [dPerms.readMessageHistory, dPerms.sendMessages, dPerms.addReactions, dPerms.manageRoles, dPerms.manageChannels, dPerms.viewChannel],
+        oAuth2Links: [],
+      },
+      'faction-disband': {
+        description: 'Kick everyone from your faction and disband it.',
+        discordPerms: [dPerms.readMessageHistory, dPerms.sendMessages, dPerms.addReactions, dPerms.manageRoles, dPerms.manageChannels, dPerms.viewChannel],
+        oAuth2Links: [],
+      },
+    }
+  },
   'nitrado-dayz': {
     name: 'Nitrado DayZ',
     botCommands: {
